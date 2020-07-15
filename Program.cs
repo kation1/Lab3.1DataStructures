5b4007a6ef2ef5feb0c8034d759059df6d2fb906
Kathryn Pidek <KathrynPidek@quickenloans.com>
2020-07-15 16:23:20 -0400

    Initial commit for lab 3.3

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace Lab3._3DataStructures
+{
+    class Program
+    {
+        static string ReverseString(string forward)
+        {  
+            //initiate stack
+            Stack wordForward = new Stack();
+            string reversedWord = "";
+           
+
+            foreach (char letter in forward)
+            {
+                //use stack to store characters
+                wordForward.Push(letter);
+            }
+
+            for (int index = 0; index < forward.Length; index++)
+            {
+                char revLetter = (char)wordForward.Pop();
+                reversedWord += revLetter;
+            }
+           
+            return reversedWord;
+
+
+        }
+        static void Main(string[] args)
+        {
+            //Request and take user imput
+            Console.WriteLine("Enter a phrase to be reversed");
+            string forward = Console.ReadLine();
+
+            //Seperate method that with string as return and parameter
+            Console.WriteLine(ReverseString(forward));
+            
+        }
+    }
+}
